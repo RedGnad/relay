db4b4467dd5b101137d9e2d8cf069be4d022fba5
Red.G <red.g@MBP-de-RedG.lan>
2025-03-02 02:23:35 +0100

    route

--- app/api/relayInteraction/route.ts
+++ app/api/relayInteraction/route.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
 import { CONTRACT_ABI, CONTRACT_ADDRESS } from "@/constant/contract";
 import { NextResponse } from "next/server";
 import { Chain, createWalletClient, getContract, http } from "viem";
